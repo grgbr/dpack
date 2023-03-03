5e2ca4ca775fd4b265d0877c038289dfb9b40682
Loïc Jourdheuil Sellin <46419549+Geneo-5@users.noreply.github.com>
2023-03-03 17:19:00 +0100

    yang2dpack: Add scripts and yang module for test
    The test is base on syslog module:
     - use ietf-syslog
     - exclude features:
       * signed-messages
       * select-match
       * select-adv-compare
     - use deviation module to remove tls case.

---
+++ scripts/yang2dpack.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+
+topdir=$(realpath $(dirname $0)/..)
+builddir=${topdir}/build
+venv=${builddir}/venv
+yang2dpack=${venv}/bin/yang2dpack
+
+
+
+if [ ! -d "${builddir}" ]; then
+        mkdir -p ${builddir}
+fi
+
+if [ ! -f "${yang2dpack}" ]; then
+        if [ -z "${PYTHON}" ]; then
+                PYTHON=$(which python3)
+        fi
+        ${PYTHON} -m venv ${venv}
+        ${venv}/bin/python -m pip install --upgrade pip
+        ${venv}/bin/pip install git+https://github.com/mbj4668/pyang#egg=pyang
+        ${venv}/bin/pip install -e ${topdir}/python
+fi
+
+PYTHONPYCACHEPREFIX=${builddir} exec ${yang2dpack} $@
