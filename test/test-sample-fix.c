037de5699422801692f21eb661d5abb4f04411b7
Loïc Jourdheuil Sellin <46419549+Geneo-5@users.noreply.github.com>
2023-03-10 17:53:59 +0100

    add setter, getter and checker (range in scalar type)

--- test/test-sample-fix.c
+++ test/test-sample-fix.c
@@ -2,32 +2,86 @@
 #include "test.h"
 
 static int
-pack(struct dpack_encoder * encoder)
+pack(struct dpack_encoder *encoder)
 {
-	const struct fix_sample spl = {
-		.eight      = 2,
-		.sixteen    = 3,
-		.thirty_two = 4
-	};
+	int                err;
+	struct fix_sample *spl;
 
-	return fix_sample_pack(encoder, &spl);
+	spl = fix_sample_create();
+	if (spl == NULL)
+		return -1;
+
+	err = fix_sample_check_eight(2);
+	if (err)
+		goto error_pack;
+
+	err = fix_sample_set_eight(spl, 2);
+	if (err)
+		goto error_pack;
+
+	err = fix_sample_check_sixteen(3);
+	if (err)
+		goto error_pack;
+
+	err = fix_sample_set_sixteen(spl, 3);
+	if (err)
+		goto error_pack;
+
+	/* try invalid range */
+	err = fix_sample_check_thirty_two(4000);
+	if (!err)
+		goto error_pack;
+
+	err = fix_sample_check_thirty_two(4);
+	if (err)
+		goto error_pack;
+
+	err = fix_sample_set_thirty_two(spl, 4);
+	if (err)
+		goto error_pack;
+
+	err = fix_sample_pack(encoder, spl);
+
+error_pack:
+	fix_sample_destroy(spl);
+	return err;
 }
 
 static int
-unpack(struct dpack_decoder * decoder)
+unpack(struct dpack_decoder *decoder)
 {
 	struct fix_sample spl;
 	int               err;
+	uint8_t         eight;
+	uint16_t      sixteen;
+	uint32_t   thirty_two;
+
+	err = fix_sample_init(&spl);
+	if (err)
+		return err;
 
 	err = fix_sample_unpack(decoder, &spl);
 	if (err)
 		return err;
 
-	printf("eight: %hhu\nsixteen: %hu\nthirty_two: %u\n",
-	       spl.eight,
-	       spl.sixteen,
-	       spl.thirty_two);
+	err = fix_sample_get_eight(&spl, &eight);
+	if (err)
+		return err;
 
+	err = fix_sample_get_sixteen(&spl, &sixteen);
+	if (err)
+		return err;
+
+	err = fix_sample_get_thirty_two(&spl, &thirty_two);
+	if (err)
+		return err;
+
+	printf("eight: %hhu\nsixteen: %hu\nthirty_two: %u\n",
+	       eight,
+	       sixteen,
+	       thirty_two);
+
+	fix_sample_fini(&spl);
 	return 0;
 }
 
