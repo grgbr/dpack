88a3bee6f1b85f33b202695c343805a4c4e14fde
Gregor Boirie <gregor.boirie@free.fr>
2023-03-14 16:14:16 +0100

    array: size computation cleanup

--- include/dpack/array.h
+++ include/dpack/array.h
@@ -8,79 +8,130 @@
 struct dpack_encoder;
 struct dpack_decoder;
 
+/* Maximum number of elements of a dpack array */
 #define DPACK_ARRAY_ELMNR_MAX (1024U)
+/* Maximum size of a dpack array */
 #define DPACK_ARRAY_SIZE_MAX  ((size_t)4 * 1024U * 1024U)
+
+/******************************************************************************
+ * Msgpack fixarray definitions
+ ******************************************************************************/
 
 /* Maximum number of elements an msgpack fixarray may encode */
 #define DPACK_FIXARRAY_ELMNR_MAX (15U)
 /* Compute size of an encoded msgpack fixarray */
 #define DPACK_FIXARRAY_SIZE(_elm_size, _elm_nr) \
 	(1UL + ((_elm_size) * (_elm_nr)))
-/* Compute the maximum size of the data portion of an encoded msgpack array */
+/* Compute the maximum size of the data portion of an encoded msgpack array. */
 #define DPACK_ARRAY_DATA_SIZE_MAX \
 	(DPACK_ARRAY_SIZE_MAX - 1UL)
 
-#define DPACK_FIXARRAY_CONST_SIZE(_elm_size, _elm_nr) \
+#define _DPACK_ARRAY_CONST_SIZE(_elm_size, _elm_nr) \
 	DPACK_FIXARRAY_SIZE(_elm_size, _elm_nr)
 
-#define DPACK_ARRAY_EVAL_CONST_SIZE(_elm_size, _elm_nr) \
-	DPACK_FIXARRAY_CONST_SIZE(_elm_size, _elm_nr)
+/******************************************************************************
+ * Msgpack 16 bits array definitions
+ ******************************************************************************/
 
 #if DPACK_ARRAY_ELMNR_MAX > DPACK_FIXARRAY_ELMNR_MAX
 
-#define DPACK_ARRAY16_ELMNR_MAX                 (UINT16_MAX)
-#define DPACK_ARRAY16_SIZE(_elm_size, _elm_nr)  (3UL + \
-                                                 ((_elm_size) * (_elm_nr)))
+/* Maximum number of elements a 16 bits msgpack array may encode */
+#define DPACK_ARRAY16_ELMNR_MAX \
+	(UINT16_MAX)
+/* Compute size of an encoded 16 bits msgpack array */
+#define DPACK_ARRAY16_SIZE(_elm_size, _elm_nr) \
+	(3UL + ((_elm_size) * (_elm_nr)))
+/* Maximum size of the data portion of an encoded 16 bits msgpack array. */
 #undef  DPACK_ARRAY_DATA_SIZE_MAX
-#define DPACK_ARRAY_DATA_SIZE_MAX               (DPACK_ARRAY_SIZE_MAX - 3UL)
+#define DPACK_ARRAY_DATA_SIZE_MAX \
+	(DPACK_ARRAY_SIZE_MAX - 3UL)
 
+/*
+ * Size of an encoded array when maximum number of elements fits into a 16 bits
+ * msgpack array.
+ */
 #define DPACK_ARRAY16_CONST_SIZE(_elm_size, _elm_nr) \
 	(((_elm_nr) > DPACK_FIXARRAY_ELMNR_MAX) ? \
 	 DPACK_ARRAY16_SIZE(_elm_size, _elm_nr) : \
-	 DPACK_FIXARRAY_CONST_SIZE(_elm_size, _elm_nr))
+	 DPACK_FIXARRAY_SIZE(_elm_size, _elm_nr))
 
-#undef DPACK_ARRAY_EVAL_CONST_SIZE
-#define DPACK_ARRAY_EVAL_CONST_SIZE(_elm_size, _elm_nr) \
+#undef _DPACK_ARRAY_CONST_SIZE
+#define _DPACK_ARRAY_CONST_SIZE(_elm_size, _elm_nr) \
 	DPACK_ARRAY16_CONST_SIZE(_elm_size, _elm_nr)
+
+/******************************************************************************
+ * Msgpack 32 bits array definitions
+ ******************************************************************************/
 
 #elif DPACK_ARRAY_ELMNR_MAX > DPACK_ARRAY16_ELMNR_MAX
 
+/* Maximum number of elements a 32 bits msgpack array may encode */
 #define DPACK_ARRAY32_ELMNR_MAX                 (UINT32_MAX)
+/* Compute size of an encoded 32 bits msgpack array */
 #define DPACK_ARRAY32_SIZE(_elm_size, _elm_nr)  (5UL + \
                                                  ((_elm_size) * (_elm_nr)))
+/* Maximum size of the data portion of an encoded 32 bits msgpack array. */
 #undef  DPACK_ARRAY_DATA_SIZE_MAX
-#define DPACK_ARRAY_DATA_SIZE_MAX               (DPACK_ARRAY_SIZE_MAX - 5UL)
+#define DPACK_ARRAY_DATA_SIZE_MAX \
+	(DPACK_ARRAY_SIZE_MAX - 5UL)
 
+/*
+ * Size of an encoded array when maximum number of elements fits into a 32 bits
+ * msgpack array.
+ */
 #define DPACK_ARRAY32_CONST_SIZE(_elm_size, _elm_nr) \
 	(((_elm_nr) > DPACK_ARRAY16_ELMNR_MAX) ? \
 	 DPACK_ARRAY32_SIZE(_elm_size, _elm_nr) : \
 	 DPACK_ARRAY16_CONST_SIZE(_elm_size, _elm_nr))
 
-#undef DPACK_ARRAY_EVAL_CONST_SIZE
-#define DPACK_ARRAY_EVAL_CONST_SIZE(_elm_size, _elm_nr) \
+#undef _DPACK_ARRAY_CONST_SIZE
+#define _DPACK_ARRAY_CONST_SIZE(_elm_size, _elm_nr) \
 	DPACK_ARRAY32_CONST_SIZE(_elm_size, _elm_nr), \
 
+/*
+ * Msgpack cannot encode array containing more than UINT32_MAX elements.
+ */
 #elif DPACK_ARRAY_ELMNR_MAX > UINT32_MAX
 
 #error msgpack cannot encode array which length is > UINT32_MAX !
 
 #endif
 
-#define DPACK_ARRAY_ELMSIZE_MAX                 (DPACK_ARRAY_DATA_SIZE_MAX / 2U)
+/*
+ * Maximum size of an array element.
+ * Set this to an arbitrary limit of one half of the maximum size of the data
+ * portion of an encoded msgpack array.
+ */
+#define DPACK_ARRAY_ELMSIZE_MAX \
+	(DPACK_ARRAY_DATA_SIZE_MAX / 2U)
 
+/*
+ * Given the size of an element and the number of elements that an array may
+ * contain, compute the size of the corresponding encoded msgpack array.
+ *
+ * Argument values MUST be known at compile time, i.e., constant. Result is
+ * unpredictable otherwise.
+ */
 #define DPACK_ARRAY_CONST_SIZE(_elm_size, _elm_nr) \
 	compile_eval(((_elm_size) > 0) && \
 	             ((_elm_size) < DPACK_ARRAY_ELMSIZE_MAX) && \
 	             ((_elm_nr) > 0) && \
 	             ((_elm_nr) <= DPACK_ARRAY_ELMNR_MAX) && \
 	             ((_elm_size) <= (DPACK_ARRAY_SIZE_MAX / (_elm_nr))), \
-	             DPACK_ARRAY_EVAL_CONST_SIZE(_elm_size, _elm_nr), \
-	             "Invalid constant array size")
+	             _DPACK_ARRAY_CONST_SIZE(_elm_size, _elm_nr), \
+	             "invalid constant array size")
 
+/*
+ * Given the size of an element and the number of elements that an array may
+ * contain, compute the size of the corresponding encoded msgpack array.
+ *
+ * Argument values MUST be known at compile time, i.e., constant. Trigger a
+ * compile time error otherwise.
+ */
 #define DPACK_ARRAY_SIZE(_elm_size, _elm_nr) \
 	compile_eval(__builtin_constant_p((_elm_size) * (_elm_nr)), \
 	             DPACK_ARRAY_CONST_SIZE(_elm_size, _elm_nr), \
-	             "Constant array size expected")
+	             "constant array size expected")
 
 #define DPACK_ARRAY_BOOL_SIZE(_elm_nr) \
 	DPACK_ARRAY_SIZE(DPACK_BOOL_SIZE, _elm_nr)
