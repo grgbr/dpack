d4b324224c3fa4f8edecdd8fc346ae9c5d90846a
Loïc Jourdheuil Sellin <46419549+Geneo-5@users.noreply.github.com>
2023-03-16 15:48:30 +0100

    afl: add afl++ simple test

---
+++ afl/test-afl.c
@@ -0,0 +1,127 @@
+#include <afl-fix.h>
+#include <dpack/codec.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+__AFL_FUZZ_INIT();
+
+#define STRING_VALUE "test string"
+
+static int
+save_to_file(const char *path, const char *buffer, size_t size)
+{
+	assert(path);
+	assert(buffer);
+	assert(size);
+
+	FILE       *out;
+	const char *msg;
+	int         err = 0;
+
+	out = fopen(path, "w");
+	if (!out) {
+		msg = "opening output file failed";
+		err = errno;
+		goto err;
+	}
+
+	if (fwrite(buffer, size, 1, out) != 1) {
+		msg = "saving data failed";
+		err = errno;
+	}
+
+	if (fclose(out)) {
+		if (!err) {
+			msg = "closing output file failed";
+			err = errno;
+		}
+	}
+
+	if (!err)
+		return 0;
+
+err:
+	return -err;
+}
+
+static int
+pack_to_file(const char *path)
+{
+	char                  *buff;
+	struct dpack_encoder   enc;
+	int                    err;
+	struct afl_sample      spl;
+	char                  *string;
+
+
+
+	buff = malloc(AFL_SAMPLE_PACKED_SIZE_MAX);
+	if (!buff)
+		return EXIT_FAILURE;
+
+	string = malloc(sizeof(STRING_VALUE));
+	if (!string)
+		return EXIT_FAILURE;
+
+	strncpy(string, STRING_VALUE, sizeof(STRING_VALUE));
+
+	dpack_init_buffer_encoder(&enc, buff, AFL_SAMPLE_PACKED_SIZE_MAX);
+	err |= afl_sample_init(&spl);
+	err |= afl_sample_set_string(&spl, string);
+	err |= afl_sample_pack(&enc, &spl);
+	err |= save_to_file(path, buff, dpack_encoder_space_used(&enc));
+	dpack_exit_encoder(&enc);
+	afl_sample_fini(&spl);
+
+	free(buff);
+
+	return err ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+
+int main(int argc, char * const argv[])
+{
+	struct dpack_decoder   dec;
+	struct dpack_encoder   enc;
+	struct afl_sample      spl;
+	char                  *out;
+	int                    ret;
+
+
+	if (argc == 2)
+		return pack_to_file(argv[1]);
+
+	out = malloc(AFL_SAMPLE_PACKED_SIZE_MAX);
+	if (!out)
+		return EXIT_FAILURE;
+
+	dpack_init_buffer_encoder(&enc, out, AFL_SAMPLE_PACKED_SIZE_MAX);
+	ret = afl_sample_init(&spl);
+	if (ret)
+		return EXIT_FAILURE;
+
+#ifdef __AFL_HAVE_MANUAL_CONTROL
+	__AFL_INIT();
+#endif
+	unsigned char *buf = __AFL_FUZZ_TESTCASE_BUF;
+
+	while (__AFL_LOOP(10000)) {
+		size_t len = __AFL_FUZZ_TESTCASE_LEN;
+
+		if (len < AFL_SAMPLE_PACKED_SIZE_MIN)
+			continue;
+
+		if (len > AFL_SAMPLE_PACKED_SIZE_MAX)
+			continue;
+
+		dpack_init_buffer_decoder(&dec, (const char *)buf, len);
+		ret = afl_sample_unpack(&dec, &spl);
+		if (!ret)
+			ret = afl_sample_pack(&enc, &spl);
+	}
+
+	dpack_exit_decoder(&dec);
+	dpack_exit_encoder(&enc);
+	afl_sample_fini(&spl);
+
+	return 0;
+}
---
+++ scripts/afl.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -e
+
+topdir=$(realpath $(dirname $0)/..)
+test_afl=${topdir}/build/test-afl
+input=${topdir}/build/input
+output=${topdir}/build/afl
+
+rm -rf $output
+rm -rf $input
+
+mkdir -p $input
+$test_afl $input/sample
+
+# export AFL_DEBUG=1
+
+tmux new-session -d -s real
+
+## Create the windows on which each node or .launch file is going to run
+tmux send-keys -t real 'tmux rename-window whatsup ' ENTER
+tmux send-keys -t real 'tmux new-window -n main '    ENTER
+tmux send-keys -t real 'tmux new-window -n fast '    ENTER
+tmux send-keys -t real 'tmux new-window -n exploit ' ENTER
+tmux send-keys -t real 'tmux new-window -n explore ' ENTER
+tmux send-keys -t real 'tmux new-window -n seek '    ENTER
+tmux send-keys -t real 'tmux new-window -n rare '    ENTER
+tmux send-keys -t real 'tmux new-window -n mmopt '   ENTER
+tmux send-keys -t real 'tmux new-window -n coe '     ENTER
+tmux send-keys -t real 'tmux new-window -n lin '     ENTER
+tmux send-keys -t real 'tmux new-window -n quad '    ENTER
+tmux send-keys -t real 'tmux new-window -n lfast1 '  ENTER
+tmux send-keys -t real 'tmux new-window -n lfast2 '  ENTER
+tmux send-keys -t real 'tmux new-window -n lfast3 '  ENTER
+tmux send-keys -t real 'tmux new-window -n lfast4 '  ENTER
+tmux send-keys -t real 'tmux new-window -n lfast5 '  ENTER
+
+
+## Send the command to each window from window 0
+tmux send-keys -t real "tmux send-keys -t main    'AFL_IMPORT_FIRST=1 afl-fuzz -D -Z -i $input -o $output            -M main    $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t fast    'AFL_IMPORT_FIRST=1 afl-fuzz       -i $input -o $output -p fast    -S fast    $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t lfast1  'AFL_IMPORT_FIRST=1 afl-fuzz -L 0  -i $input -o $output -p fast    -S lfast1  $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t lfast2  'AFL_IMPORT_FIRST=1 afl-fuzz -L 0  -i $input -o $output -p fast    -S lfast2  $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t lfast3  'AFL_IMPORT_FIRST=1 afl-fuzz -L 0  -i $input -o $output -p fast    -S lfast3  $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t lfast4  'AFL_IMPORT_FIRST=1 afl-fuzz -L 0  -i $input -o $output -p fast    -S lfast4  $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t lfast5  'AFL_IMPORT_FIRST=1 afl-fuzz -L 0  -i $input -o $output -p fast    -S lfast5  $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t exploit 'AFL_IMPORT_FIRST=1 afl-fuzz       -i $input -o $output -p exploit -S exploit $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t explore 'AFL_IMPORT_FIRST=1 afl-fuzz       -i $input -o $output -p explore -S explore $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t seek    'AFL_IMPORT_FIRST=1 afl-fuzz       -i $input -o $output -p seek    -S seek    $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t rare    'AFL_IMPORT_FIRST=1 afl-fuzz       -i $input -o $output -p rare    -S rare    $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t mmopt   'AFL_IMPORT_FIRST=1 afl-fuzz       -i $input -o $output -p mmopt   -S mmopt   $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t coe     'AFL_IMPORT_FIRST=1 afl-fuzz       -i $input -o $output -p coe     -S coe     $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t lin     'AFL_IMPORT_FIRST=1 afl-fuzz       -i $input -o $output -p lin     -S lin     $test_afl' ENTER" ENTER
+tmux send-keys -t real "tmux send-keys -t quad    'AFL_IMPORT_FIRST=1 afl-fuzz       -i $input -o $output -p quad    -S quad    $test_afl' ENTER" ENTER
+
+tmux send-keys -t real "tmux send-keys -t whatsup  'while true; do afl-whatsup -s $output/; sleep 10; clear; done' ENTER" ENTER
+tmux send-keys -t real "tmux select-window -t whatsup " ENTER
+
+## Attach to session
+tmux attach -t real
